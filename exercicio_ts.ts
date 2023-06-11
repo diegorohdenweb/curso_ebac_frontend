7debd106d1510e0b3390e1f304f252dc6ece200b
diegorohdenweb <111529530+diegorohdenweb@users.noreply.github.com>
2023-06-11 10:40:33 -0300

    Commit ts

---
+++ exercicio_ts.ts
@@ -0,0 +1,9 @@
+// função de multiplicação
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+// função de saudação
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
